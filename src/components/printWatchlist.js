209a8feb50194e85df514d598d479b696d5f88cd
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-18 21:44:32 +0530

    Add the empty wtchlist function.

--- src/components/printWatchlist.js
+++ src/components/printWatchlist.js
@@ -1,4 +1,12 @@
 const printWatchlist = () => {
+  const emptyWatchList = document.createElement('h5');
+  emptyWatchList.className = 'empty';
+  emptyWatchList.innerHTML = '<a class="empty">Empty watch List</a>';
+  document.querySelector('.header').appendChild(emptyWatchList);
+  document.querySelector('.empty').addEventListener('click', () => {
+    localStorage.removeItem('idArray');
+    window.location.reload();
+  });
   document.querySelector('#watchLater').style.display = 'none';
   const table = document.querySelector('#animes');
   for (let i = 1; i < table.rows.length; i += 1) {
